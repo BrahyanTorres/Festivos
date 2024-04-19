bc24104746feea949b4a9e8e76e816bf54982159
BrahyanTorres <143438365+BrahyanTorres@users.noreply.github.com>
2024-04-19 11:18:32 -0500

    SegundaFase

--- src/main/java/comapirest/demo/controladores/FestivoControlador.java
+++ src/main/java/comapirest/demo/controladores/FestivoControlador.java
@@ -15,21 +15,18 @@ public class FestivoControlador {
     @Autowired
     private IFestivoServicio servicio;
 
-    
     @RequestMapping(value = "/verificar/{año}/{mes}/{dia}", method = RequestMethod.GET)
     public String verificarFestivo(@PathVariable int año, @PathVariable int mes, @PathVariable int dia) {
-        if (servicio.esFechaValida(String.valueOf(año) + "-" + String.valueOf(mes) + "-" + String.valueOf(dia))) {
-            @SuppressWarnings("deprecation")
-            Date fecha = new Date(año - 1900, mes - 1, dia);
+        String fechaString = String.format("%d-%02d-%02d", año, mes, dia);
+        if (servicio.esFechaValida(fechaString)) {
+            LocalDate fecha = LocalDate.of(año, mes, dia);
             return servicio.esFestivo(fecha) ? "Es Festivo" : "No es festivo";
         } else {
-            return "Fecha No valida";
+            return "Fecha no válida";
         }
-
     }
 
     @SuppressWarnings("unchecked")
-    @CrossOrigin(origins = "*")
     @GetMapping("/obtener/{año}")
     public List<Festivos> obtener(@PathVariable int año) {
         return (List<Festivos>) servicio.obtenerFestivos(año);
